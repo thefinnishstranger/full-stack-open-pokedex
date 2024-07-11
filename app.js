c787bd889cb3ea766ccd92d056548f23844f504b
thefinnishstranger <nikolas.gustavson4@gmail.com>
2024-07-11 19:47:18 +0300

    updated the fly.toml and app.js

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.get('/version', (req, res) => {
   res.send(1)
 })
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
