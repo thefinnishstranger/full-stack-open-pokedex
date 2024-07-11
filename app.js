c77541178b3a1f2210120e6f2ec77647dcadfbfc
thefinnishstranger <nikolas.gustavson4@gmail.com>
2024-07-11 19:52:40 +0300

    updated app.js

--- app.js
+++ app.js
@@ -14,6 +14,12 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+app.get('/health', (req, res) => {
+  // eslint-disable-next-line no-constant-condition
+  if (true) throw('error...  ')
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
 })
